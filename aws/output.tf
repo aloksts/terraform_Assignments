0f9d18b7b33c1453fe49e87ebaf662ea20b95016
aloksts <65123641+aloksts@users.noreply.github.com>
2024-01-11 12:46:33 +0530

    Update output.tf

--- aws/output.tf
+++ aws/output.tf
@@ -1,3 +1,9 @@
+data "http" "htmlc" {
+  url = "https://localhost"
+  request_headers = {
+    Accept = "application/json"
+  }
+}
 output "index_html_content" {
         value = file("index.html")
     }
